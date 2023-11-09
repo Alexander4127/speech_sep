ab60a60561fc7e8df7725654fbbb5d05e3a53a85
Alexander4127 <askutsakov@gmail.com>
2023-11-09 13:55:17 +0300

    Fix checkpoint loading

--- ss/base/base_trainer.py
+++ ss/base/base_trainer.py
@@ -164,7 +164,7 @@ class BaseTrainer:
         self.logger.info("Loading checkpoint: {} ...".format(resume_path))
         checkpoint = torch.load(resume_path, self.device)
         self.start_epoch = checkpoint["epoch"] + 1
-        self.mnt_best = inf
+        self.mnt_best = checkpoint["monitor_best"]
 
         # load architecture params from checkpoint.
         if checkpoint["config"]["arch"] != self.config["arch"]:
